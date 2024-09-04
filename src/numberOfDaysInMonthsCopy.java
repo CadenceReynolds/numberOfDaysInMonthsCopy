53f3a19ecd9ba903c618d18e0679bb9192d634d7
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-09-04 13:25:51 -0400

    Finished Assignment

--- src/numberOfDaysInMonthsCopy.java
+++ src/numberOfDaysInMonthsCopy.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class numberOfDaysInMonthsCopy {
+public class numberOfDaysInMonthsCopy {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(11212));
+    }
+
+    /**
+     * returns true if number is a palindrome.
+     *
+     * @param num the number to check
+     * @return true if a palindrome. false if not
+     */
+    public static boolean isPalindrome(int num){
+        num = Math.abs(num);
+
+        int number = num;
+        int reverse = 0;
+        while(num > 0){
+            int digit = num % 10;
+            reverse *= 10;
+            reverse += digit;
+
+            num /= 10;
+        }
+        return reverse == number;
+
+    }
+
 }
